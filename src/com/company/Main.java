12c6c0cdfcd77d2197e3892580d4e129de8636a3
Evhenii <53866144+Jeid14@users.noreply.github.com>
2021-01-19 09:47:30 +0200

    WorkTable (#3)
    * addClases
    
    * WorkTable
    
    * WorkTable
    
    Co-authored-by: Yulia Nefodova <36659197+YuliaNefodova@users.noreply.github.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,18 @@ package com.company;
 
 import com.company.executor.Executable;
 import com.company.story.MainMenu;
+import com.company.visual.Table;
 
 import java.io.IOException;
 
 public class Main extends Object {
 
     public static void main(String[] args) throws IOException {
-
-
+//
+//
         MainMenu mainMenu = new MainMenu();
         mainMenu.mainMenu();
+
 
 
     }
--- src/com/company/visual/Table.java
+++ src/com/company/visual/Table.java
@@ -1,4 +1,43 @@
 package com.company.visual;
 
+import com.company.CRUD.Read;
+import com.company.executor.ExecutorFactory;
+import com.company.model.Person;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.io.IOException;
+import java.time.Period;
+import java.util.*;
+
+
 public class Table {
+
+    public void create(List<Person>personList) throws IOException {
+//        List<Person> personArrayList =new ExecutorFactory().getInstanceByFormat("json").read("1.json");;
+       // personArrayList.add(new Person(11,"33","44",55,"ii"));
+        DefaultTableModel model = new DefaultTableModel();
+        JTable table = new JTable(model);
+        model.addColumn("ID");
+        model.addColumn("FirstName");
+        model.addColumn("LastName");
+        model.addColumn("City");
+        model.addColumn("Age");
+        for(Person p:personList) {
+            model.addRow(new String[]{String.valueOf(p.getId()),p.getFirstName(),p.getLastName(),p.getCity(),String.valueOf(p.getAge())});
+        }
+        Vector data = model.getDataVector();
+        Vector row = (Vector) data.elementAt(0);
+        // Copy the first column
+        int mColIndex = 0;
+        List colData = new ArrayList(table.getRowCount());
+        for (int i = 0; i < table.getRowCount(); i++) {
+            row = (Vector) data.elementAt(i);
+            colData.add(row.get(mColIndex));
+        }
+        JFrame f = new JFrame();
+        f.setSize(600, 300);
+        f.add(new JScrollPane(table));
+        f.setVisible(true);
+    }
 }
